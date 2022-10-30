87827941dcf4f448df322e059a9a43d010f6cedd
Paul Young <youngjpj@gmail.com>
2022-10-30 18:23:39 +0000

    Change function order to improve readability

--- backup.py
+++ backup.py
@@ -1,16 +1,16 @@
 '''
-Hangman - A Python terminal game by Paul Young.
+Hangman python terminal game by Paul Young
 '''
 
 
 # Imports
+import gspread
+from google.oauth2.service_account import Credentials
 import random
 import os
 import time
 import math
 import pyfiglet
-import gspread
-from google.oauth2.service_account import Credentials
 from colorama import init, Fore
 init(autoreset=True)
 
@@ -25,8 +25,9 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('hangman_words')
-SCORE_SHEET = SHEET.worksheet('scoreboard')
-SCORE_DATA = SCORE_SHEET.get_all_values()
+
+score_sheet = SHEET.worksheet('scoreboard')
+score_data = score_sheet.get_all_values()
 
 
 # ASCII art
@@ -178,114 +179,204 @@ MENU_ART = '''
 '''
 
 
-# Game Variables
-PLAYER_LIVES = 9
-GAME_STAGE = 0
-GAME_WIN = False
-GAME_OVER = False
-GAME_WORD = None
-HIDDEN_WORD = None
-CATEGORY = None
-START_TIME = None
-END_TIME = None
-SECONDS = None
-SCORE = None
-NAME = None
-
-
-# Print Function
-def cprint(text):
+# Print functions
+def cprint(a):
     '''
     Take parameter and print it center aligned.
     '''
     terminal_width = 80
 
-    print(text.center(terminal_width))
+    print(a.center(terminal_width))
 
 
-# Game Menu
-def main_menu():
+# Game functions
+def reset_game():
     '''
-    Draw main menu.
-    Take user input and run relevant function.
+    Resets variables.
     '''
+    global player_lives, game_over, game_win, game_stage
+
+    game_stage = 0
+    game_win = False
+    game_over = False
+    player_lives = 9
+
+
+def game_win_trigger():
+    '''
+    Function called when the player has won the game.
+    From guessing the word, or guessing every letter.
+    '''
+    global player_lives, game_stage, game_over, end_time, game_win
+
+    end_time = time.time()
+    game_win = True
+    game_over = True
+    game_display(WIN_HEADER)
+
+
+def set_word():
+    '''
+    Takes input from user to select a category.
+    Picks a random word from Google sheet.
+    Makes string of _ based on length of random word.
+    '''
+    global game_word, hidden_word, category
+
     os.system('clear')
 
-    print(GAME_HEADER)
-    cprint('WELCOME TO HANGMAN!')
-    cprint('===================')
-    print(MENU_ART)
-    l_text = '1. Play Hangman'
-    mid_text = '2. How To Play'
-    mid_text_2 = '3. High Scores'
-    r_text = '4. Last 5 Scores'
-    print(f'{l_text : <20}{mid_text : ^20}{mid_text_2 : ^20}{r_text : >20}')
+    print(pyfiglet.figlet_format('Category', justify='center', width=80))
+    print('1: Halloween')
+    print('2: Pokemon')
+    print('3: Countries')
+    print('4: Animals')
+    print('5: Disney Movies')
+    print('\n' * 12)
     print('-' * 80)
-    choice = input('Select an option: ')
-    valid_choices = ['1', '2', '3', '4']
+    category = input('Select a category: ')
 
-    if choice not in valid_choices:
+    valid_choices = ['1', '2', '3', '4', '5']
+    word_sheet = SHEET.worksheet('word_sheet')
+
+    if category not in valid_choices:
         print(f'{Fore.RED}Invalid selection, try again.')
         time.sleep(1)
-        main_menu()
-    elif choice == '1':
-        start_game()
-    elif choice == '2':
-        instructions()
-    elif choice == '3':
-        print('Loading scoreboard...')
-        highscores()
-    elif choice == '4':
-        print('Loading scoreboard...')
-        last_five_scores()
+        set_word()
+    elif category == '1':
+        word_list = word_sheet.col_values(1)
+        category = 'Halloween Word'
+    elif category == '2':
+        word_list = word_sheet.col_values(2)
+        category = 'Pokemon'
+    elif category == '3':
+        word_list = word_sheet.col_values(3)
+        category = 'Country'
+    elif category == '4':
+        word_list = word_sheet.col_values(4)
+        category = 'Animal'
+    elif category == '5':
+        word_list = word_sheet.col_values(5)
+        category = 'Disney Movie'
+
+    game_word = random.choice(word_list)
+    hidden_word = '_' * len(game_word)
+
+    print('Loading game...')
 
 
-# Game Instructions
-def instructions():
+def user_input():
     '''
-    Print game instructions.
+    While game is running:
+    - Displays guessed letters and player health.
+    - Takes input from player as guess.
+    - Passes valid guess to check_guess function.
+    - Calls game_display to redraw game.
     '''
-    os.system('clear')
+    global game_over, game_win, player_lives, start_time
 
-    print()
-    print(pyfiglet.figlet_format('How To Play', justify='center', width=80))
-    cprint('The goal of Hangman is to solve the mystery word.\n')
-    cprint('_ _ _ _ _ _ _\n')
-    cprint('Guess one letter at the time.')
-    cprint('If you guess correctly, the letter will appear in the word.\n')
-    cprint('H A _ G _ A _\n')
-    cprint('If you guess incorrectly, the stage will progress.')
-    cprint('After 9 incorrect guesses the game will be over.\n')
-    cprint('At any point you can guess the whole word.\n')
-    cprint('H A N G M A N')
-    print('\n')
+    guessed_letters = []
+    start_time = time.time()
 
-    bottom_input()
+    while game_over is False:
+
+        guessed_letters_str = ' '.join(guessed_letters)
+        life_bar = ' ♥' * player_lives
+
+        print(f'Guessed letters: {guessed_letters_str : <45}{life_bar : >18}')
+        print('-' * 80)
+        guess = input('Guess a letter OR the word: ').upper()
+
+        def redraw():
+            time.sleep(1)
+            game_display(GAME_HEADER)
+
+        if guess == 'HELP':
+            print(game_word)
+            redraw()
+        elif guess == game_word:
+            game_win_trigger()
+        elif not guess.isalpha() or len(guess) > 1:
+            print(f'{Fore.RED}Invalid guess')
+            redraw()
+        elif guess in guessed_letters:
+            print(f'{Fore.YELLOW}Letter already guessed, try another')
+            redraw()
+        else:
+            guessed_letters.append(guess)
+            guessed_letters.sort()
+            check_guess(guess)
 
 
-# Scoreboard
-def last_five_scores():
+def check_guess(guess):
     '''
-    Slices last 5 newest scores from sheet.
-    Sorts scores in descending order.
-    Passes to draw_table function.
+    Checks for correct or incorrect guess.
+    Updates varibles as necessary.
+    Calls game_display to redraw game.
     '''
+    global player_lives, game_stage, game_over, end_time
 
-    score_slice = SCORE_DATA[-1:-6:-1]
-    score_slice = sorted(score_slice, key=lambda x: int(x[1]), reverse=True)
+    if guess in game_word:
+        print(f'{Fore.GREEN}Correct guess!')
+        update_hidden_word(guess)
+    elif guess not in game_word and game_stage != 8:
+        print(f'{Fore.RED}Incorrect guess!')
+        player_lives -= 1
+        game_stage += 1
+    else:
+        # Game fail trigger
+        end_time = time.time()
+        player_lives -= 1
+        game_stage += 1
+        game_over = True
+        game_display(FAIL_HEADER)
 
-    draw_table(score_slice, 'Latest Scores', 'Last 5 Scores:')
+    time.sleep(1)
+
+    if game_over is False:
+        game_display(GAME_HEADER)
 
 
-def highscores():
+def update_hidden_word(guess):
     '''
-    Sorts scores in descending order.
-    Passes top 5 to draw_table function.
+    Takes a correct guess and updates hidden_word.
+    If hidden_word complete -> game_over = True.
     '''
+    global hidden_word, game_over, game_win, end_time
 
-    score_sorted = sorted(SCORE_DATA, key=lambda x: int(x[1]), reverse=True)
+    positions = [i for i, a in enumerate(game_word) if a == guess]
+    hidden_word_arr = list(hidden_word)
 
-    draw_table(score_sorted[:5], 'High Scores', 'All Time Top 5 Scores:')
+    for num in positions:
+        hidden_word_arr[num] = guess
+
+    hidden_word = ''.join(hidden_word_arr)
+
+    if '_' not in hidden_word:
+        game_win_trigger()
+
+
+def calculate_score():
+    '''
+    Calculates and prints player score based on lives, word length, and time.
+    '''
+    global seconds, score
+
+    seconds = math.floor(end_time - start_time)
+    score = math.ceil((len(game_word) * 500) + (player_lives * 1000) / seconds)
+    left_text = 'Congratulations!'
+    mid_text = f'SCORE: {score}'
+    right_text = f'TIME: {seconds}s'
+
+    print(f'{left_text : <30}{mid_text : ^20}{right_text : >30}')
+
+
+def bottom_input():
+    '''
+    Prints a line of '-' and waits for user input before returning to the menu.
+    '''
+    print('-' * 80)
+    input('Press ENTER to return to the menu...')
+    main_menu()
 
 
 def draw_table(scores, heading, heading2):
@@ -315,79 +406,55 @@ def draw_table(scores, heading, heading2):
         rank += 1
 
     print('\n' * 2)
-
     bottom_input()
 
 
-# Main Game Functions
-def start_game():
+def last_five_scores():
     '''
-    Main game function.
+    Slices last 5 scores from sheet.
+    Sorts scores in descending order.
+    A for loop prints the array values line by line.
     '''
-    reset_game()
-    set_word()
-    game_display(GAME_HEADER)
-    user_input()
+
+    score_slice = score_data[-1:-6:-1]
+    score_slice = sorted(score_slice, key=lambda x: int(x[1]), reverse=True)
+
+    draw_table(score_slice, 'Latest Scores', 'Last 5 Scores:')
 
 
-def reset_game():
+def highscores():
     '''
-    Resets variables.
+    Slices last 5 scores from sheet.
+    Sorts scores in descending order.
+    A for loop prints the array values line by line.
     '''
-    global PLAYER_LIVES, GAME_STAGE, GAME_WIN, GAME_OVER
 
-    PLAYER_LIVES = 9
-    GAME_STAGE = 0
-    GAME_WIN = False
-    GAME_OVER = False
+    score_sorted = sorted(score_data, key=lambda x: int(x[1]), reverse=True)
+
+    draw_table(score_sorted[:5], 'High Scores', 'All Time Top 5 Scores:')
 
 
-def set_word():
+def update_scoreboard():
     '''
-    Takes input from user to select a category.
-    Picks a random word from Google sheet.
-    Makes string of underscores based on length of random word.
+    Updates scoreboard sheet.
     '''
-    global GAME_WORD, CATEGORY
+    global name
 
-    os.system('clear')
+    while True:
+        name = input('Please enter your name: ').capitalize()[:10]
+        if not name.isalpha():
+            print(f'{Fore.RED}Invalid name, try again...')
+        elif name == '':
+            print(f'{Fore.RED}Invalid name, try again...')
+        else:
+            break
 
-    print(pyfiglet.figlet_format('Category', justify='center', width=80))
-    print('1: Halloween')
-    print('2: Pokemon')
-    print('3: Countries')
-    print('4: Animals')
-    print('5: Disney Movies')
-    print('\n' * 12)
-    print('-' * 80)
-    choice = input('Select a category: ')
+    print('Updating scoreboard...')
 
-    valid_choices = ['1', '2', '3', '4', '5']
-    word_sheet = SHEET.worksheet('word_sheet')
+    score_sheet = SHEET.worksheet('scoreboard')
+    score_sheet.append_row([name, score, seconds, game_word])
 
-    if choice not in valid_choices:
-        print(f'{Fore.RED}Invalid selection, try again.')
-        time.sleep(1)
-        set_word()
-    elif choice == '1':
-        word_list = word_sheet.col_values(1)
-        CATEGORY = 'Halloween Word'
-    elif choice == '2':
-        word_list = word_sheet.col_values(2)
-        CATEGORY = 'Pokemon'
-    elif choice == '3':
-        word_list = word_sheet.col_values(3)
-        CATEGORY = 'Country'
-    elif choice == '4':
-        word_list = word_sheet.col_values(4)
-        CATEGORY = 'Animal'
-    elif choice == '5':
-        word_list = word_sheet.col_values(5)
-        CATEGORY = 'Disney Movie'
-
-    GAME_WORD = random.choice(word_list)
-
-    print('Loading game...')
+    end_screen()
 
 
 def game_display(header):
@@ -398,194 +465,107 @@ def game_display(header):
     - Current game stage.
     At the end of a game, shows text based on win or fail.
     '''
-    global HIDDEN_WORD
 
     os.system('clear')
 
-    HIDDEN_WORD = '_' * len(GAME_WORD)
-
     print(header)
-    cprint(f'Mystery {CATEGORY}:')
+    cprint(f'Mystery {category}:')
     print()
-    cprint(f'{HIDDEN_WORD} ({len(GAME_WORD)})')
-    print(HANGMAN_STAGES[GAME_STAGE])
+    cprint(f'{hidden_word} ({len(game_word)})')
+    print(HANGMAN_STAGES[game_stage])
 
-    if GAME_WIN:
+    if game_win:
         calculate_score()
         print('-' * 80)
         update_scoreboard()
-    elif GAME_OVER and GAME_WIN is False:
+    elif game_over and game_win is False:
         left_text = 'Oh dear he died!'
-        right_text = f'The Mystery {CATEGORY} was {GAME_WORD}.'
+        right_text = f'The Mystery {category} was {game_word}.'
         print(f'{left_text : <40}{right_text : >40}')
-
         bottom_input()
-
-
-def user_input():
-    '''
-    While game is running:
-    - Displays guessed letters and player health.
-    - Takes input from player as guess.
-    - Passes valid guess to check_guess function.
-    - Passes correct word guess to game_win_trigger function.
-    - Calls game_display to redraw game.
-    '''
-    global START_TIME
-
-    guessed_letters = []
-    START_TIME = time.time()
-
-    while GAME_OVER is False:
-
-        guessed_letters_str = ' '.join(guessed_letters)
-        life_bar = ' ♥' * PLAYER_LIVES
-
-        print(f'Guessed letters: {guessed_letters_str : <45}{life_bar : >18}')
-        print('-' * 80)
-        guess = input('Guess a letter OR the word: ').upper()
-
-        def redraw():
-            time.sleep(1)
-            game_display(GAME_HEADER)
-
-        if guess == 'HELP':
-            print(GAME_WORD)
-            redraw()
-        elif guess == GAME_WORD:
-            game_win_trigger()
-        elif not guess.isalpha() or len(guess) > 1:
-            print(f'{Fore.RED}Invalid guess')
-            redraw()
-        elif guess in guessed_letters:
-            print(f'{Fore.YELLOW}Letter already guessed, try another')
-            redraw()
-        else:
-            guessed_letters.append(guess)
-            guessed_letters.sort()
-            check_guess(guess)
-
-
-def check_guess(guess):
-    '''
-    Checks for correct or incorrect guess.
-    Updates varibles as necessary.
-    Calls game_display to redraw game.
-    '''
-    global PLAYER_LIVES, GAME_STAGE, GAME_OVER, END_TIME
-
-    if guess in GAME_WORD:
-        print(f'{Fore.GREEN}Correct guess!')
-        update_hidden_word(guess)
-    elif guess not in GAME_WORD and GAME_STAGE != 8:
-        print(f'{Fore.RED}Incorrect guess!')
-        PLAYER_LIVES -= 1
-        GAME_STAGE += 1
-    else:
-        # Game fail trigger
-        END_TIME = time.time()
-        PLAYER_LIVES -= 1
-        GAME_STAGE += 1
-        GAME_OVER = True
-        game_display(FAIL_HEADER)
-
-    time.sleep(1)
-
-    if GAME_OVER is False:
-        game_display(GAME_HEADER)
-
-
-def update_hidden_word(guess):
-    '''
-    Takes a correct guess and updates hidden_word.
-    If hidden_word complete -> game_over = True.
-    '''
-    global HIDDEN_WORD
-
-    positions = [i for i, a in enumerate(GAME_WORD) if a == guess]
-    hidden_word_arr = list(HIDDEN_WORD)
-
-    for num in positions:
-        hidden_word_arr[num] = guess
-
-    HIDDEN_WORD = ''.join(hidden_word_arr)
-
-    if '_' not in HIDDEN_WORD:
-        game_win_trigger()
-
-
-def game_win_trigger():
-    '''
-    Function called when the player has won the game.
-    From guessing the word, or guessing every letter.
-    '''
-    global END_TIME, GAME_WIN, GAME_OVER
-
-    END_TIME = time.time()
-    GAME_WIN = True
-    GAME_OVER = True
-    game_display(WIN_HEADER)
-
-
-def calculate_score():
-    '''
-    Calculates and prints player score - based on lives, word length, and time.
-    '''
-    global SECONDS, SCORE
-
-    SECONDS = math.floor(END_TIME - START_TIME)
-    SCORE = math.ceil((len(GAME_WORD) * 500) + (PLAYER_LIVES * 1000) / SECONDS)
-    left_text = 'Congratulations!'
-    mid_text = f'SCORE: {SCORE}'
-    right_text = f'TIME: {SECONDS}s'
-
-    print(f'{left_text : <30}{mid_text : ^20}{right_text : >30}')
-
-
-def update_scoreboard():
-    '''
-    Updates scoreboard sheet.
-    '''
-    global NAME
-
-    while True:
-        NAME = input('Please enter your name: ').capitalize()[:10]
-        if not NAME.isalpha():
-            print(f'{Fore.RED}Invalid name, try again...')
-        elif NAME == '':  # check if this is necessary
-            print(f'{Fore.RED}Invalid name, try again...')
-        else:
-            break
-
-    print('Updating scoreboard...')
-
-    SCORE_SHEET.append_row([NAME, SCORE, SECONDS, GAME_WORD])
-
-    end_screen()
-
+        
 
 def end_screen():
     '''
     Confirms name entry into scoreboard.
     Thanks player and returns to menu after input.
     '''
+    global name
+
     os.system('clear')
 
     print('\n' * 12)
-    cprint(f'Thank you for playing {NAME}!')
+    cprint(f'Thank you for playing {name}!')
     cprint('Your name and score have been uploaded.')
     print('\n' * 9)
-
     bottom_input()
 
 
-def bottom_input():
+def instructions():
     '''
-    Prints a line of '-' and waits for user input before returning to the menu.
+    Print game instructions.
     '''
+    os.system('clear')
+
+    print()
+    print(pyfiglet.figlet_format('How To Play', justify='center', width=80))
+    cprint('The goal of Hangman is to solve the mystery word.\n')
+    cprint('_ _ _ _ _ _ _\n')
+    cprint('Guess one letter at the time.')
+    cprint('If you guess correctly, the letter will appear in the word.\n')
+    cprint('H A _ G _ A _\n')
+    cprint('If you guess incorrectly, the stage will progress.')
+    cprint('After 9 incorrect guesses the game will be over.\n')
+    cprint('At any point you can guess the whole word.\n')
+    cprint('H A N G M A N')
+    print('\n')
+    bottom_input()
+
+
+def main_menu():
+    '''
+    Draw main menu.
+    Take user input and run relevant function.
+    '''
+    os.system('clear')
+
+    print(GAME_HEADER)
+    cprint('WELCOME TO HANGMAN!')
+    cprint('===================')
+    print(MENU_ART)
+    l_text = '1. Play Hangman'
+    mid_text = '2. How To Play'
+    mid_text_2 = '3. High Scores'
+    r_text = '4. Last 5 Scores'
+    print(f'{l_text : <20}{mid_text : ^20}{mid_text_2 : ^20}{r_text : >20}')
     print('-' * 80)
-    input('Press ENTER to return to the menu...')
-    main_menu()
+    choice = input('Select an option: ')
+    valid_choices = ['1', '2', '3', '4']
+
+    if choice not in valid_choices:
+        print(f'{Fore.RED}Invalid selection, try again.')
+        time.sleep(1)
+        main_menu()
+    elif choice == '1':
+        main()
+    elif choice == '2':
+        instructions()
+    elif choice == '3':
+        print('Loading scoreboard...')
+        highscores()
+    elif choice == '4':
+        print('Loading scoreboard...')
+        last_five_scores()
+
+
+def main():
+    '''
+    Main program function.
+    '''
+    reset_game()
+    set_word()
+    game_display(GAME_HEADER)
+    user_input()
 
 
 main_menu()
